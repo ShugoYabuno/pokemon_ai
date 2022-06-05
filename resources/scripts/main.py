6a9abbeda2ff1fac6bf409520b4484efdcbb5f5b
ShugoYabuno <shugo.ybn@gmai.com>
2022-06-05 23:05:24 +0900

    update log normalize

--- resources/scripts/main.py
+++ resources/scripts/main.py
@@ -2,6 +2,8 @@ from envs.pokemon import SingleBattle, PokemonState
 import copy
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+import scipy.stats
 
 plt.rcParams['figure.subplot.left'] = 0.17
 
@@ -44,10 +46,6 @@ pokemons = [mewtwo, lugia, ho_oh, groudon, kyogre, rayquaza,
             dialga, palkia, giratina, zekrom, reshiiam, kyurem, xerneas, yveltal, zygarde,
             solgaleo, lunala, necrozma_s, necrozma_m, zacian, zamazenta, calyrex_w, calyrex_b]
 
-sum = []
-table = []
-
-
 def step(a):
     if a > 0:
         return 1
@@ -55,42 +53,61 @@ def step(a):
         return 0
 
 
-for x in pokemons:
-    sum_damage = 0
+def zscore(x, axis=None):
+    xmean = x.mean(axis=axis, keepdims=True)
+    xstd = np.std(x, axis=axis, keepdims=True)
+    zscore = (x - xmean) / xstd
+    return zscore
 
-    for y in pokemons:
-        if x.name == y.name:
-            continue
 
-        attacker = copy.deepcopy(x)
-        defender = copy.deepcopy(y)
-
-        party_x = [attacker]
-        party_y = [defender]
-        x_spd = attacker.stats.spd
-        y_spd = defender.stats.spd
-
-        singleBattle = SingleBattle(party_x, party_y)
-        inflict_damage_ratio = singleBattle.most_effective_move(0)
-        receive_damage_ratio = singleBattle.most_effective_move(1)
-        speed_correction_value = 1.2
-
-        sum_damage += (inflict_damage_ratio[1] * speed_correction_value**step(x_spd - y_spd)) / (receive_damage_ratio[1] * speed_correction_value**step(y_spd - x_spd))
-
-    sum.append([x, sum_damage / (len(pokemons) - 1)])
-
-sorted_pokemons = sorted(sum, key=lambda x: x[1])
-
-heights = []
+speed_correction_values = [1.5, 1.0]
 labels = []
-for [pokemon, ratio] in sorted_pokemons:
-    labels.append(pokemon.name)
-    heights.append(ratio)
-
-left = np.arange(len(heights))
-
+color = "rgby"
 height=0.3
 
-plt.barh(left, heights, color='r', height=height, align='center')
+for index, speed_correction_value in enumerate(speed_correction_values):
+    sum = []
+
+    for x in pokemons:
+        sum_damage = 0
+        for y in pokemons:
+            if x.name == y.name:
+                continue
+
+            attacker = copy.deepcopy(x)
+            defender = copy.deepcopy(y)
+
+            party_x = [attacker]
+            party_y = [defender]
+            x_spd = attacker.stats.spd
+            y_spd = defender.stats.spd
+
+            singleBattle = SingleBattle(party_x, party_y)
+            inflict_damage_ratio = singleBattle.most_effective_move(0)
+            receive_damage_ratio = singleBattle.most_effective_move(1)
+
+            inflict = inflict_damage_ratio[1] * speed_correction_value**step(x_spd - y_spd)
+            receive = receive_damage_ratio[1] * speed_correction_value**step(y_spd - x_spd)
+            sum_damage += math.log2(inflict / receive)
+
+        sum.append([x.name, sum_damage / (len(pokemons) - 1)])
+
+    heights = []
+    if index == 0:
+        sorted_pokemons = sorted(sum, key=lambda x: x[1])
+        labels = [pokemon[0] for pokemon in sorted_pokemons]
+        heights = [pokemon[1] for pokemon in sorted_pokemons]
+        normalized = scipy.stats.zscore(heights)
+        left = np.arange(len(normalized))
+
+        plt.barh(left, normalized, color=color[index], height=height, align='center', label=str(speed_correction_value))
+    else:
+        sorted_pokemons = sorted(sum, key=lambda x: labels.index(x[0]))
+        heights = [pokemon[1] for pokemon in sorted_pokemons]
+        normalized = scipy.stats.zscore(heights)
+        plt.barh(left-height*index, normalized, color=color[index], height=height, align='center', label=str(speed_correction_value))
+
+
 plt.yticks(left + height/2, labels)
+plt.legend()
 plt.show()
